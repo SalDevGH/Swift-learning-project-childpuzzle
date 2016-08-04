70c05bb07a5269494406262efb7c1feb1877b8ab
DevLab <devlab.couk@gmail.com>
2016-08-04 22:10:48 +0200

    added unit test for verifying distance calculation logic

--- childPuzzle/childPuzzleTests/childPuzzleTests.swift
+++ childPuzzle/childPuzzleTests/childPuzzleTests.swift
@@ -10,27 +10,64 @@ import XCTest
 @testable import childPuzzle
 
 class childPuzzleTests: XCTestCase {
-    
+
+	var matchingItemController:MatchingItemController?
+	var matchingItemData:MatchingItemModel.matchingItemData?
+
+	// called before the invocation of each test method in the class
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+		self.matchingItemController = MatchingItemController()
+		self.matchingItemData       = MatchingItemModel.matchingItemData(
+			idPlayItem: 0,
+			idStage: 1,
+			alias: "",
+			nodeName: "",
+			width: 100.0,
+			height: 100.0,
+			winningPosition: CGPointMake(300, 300),
+			startPosition: CGPointMake(0, 0),
+			zPosition: 0,
+			playable: true,
+			userData: nil
+		)
     }
-    
+
+	// called after the invocation of each test method in the class
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+		self.matchingItemController = nil
+		self.matchingItemData       = nil
+
         super.tearDown()
     }
 
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-    func testRandomItemPlacingAlignerMethod() {
-//        alignRandomPositionValue
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+    // Testing if distance calculator is working correctly
+    func testIsPositionNearEnoughToWin() {
+		XCTAssertFalse(self.matchingItemController!.isPositionNearEnoughToWin(
+			CGPointMake(-10, -10),
+			itemData: self.matchingItemData!)
+		)
+		XCTAssertFalse(self.matchingItemController!.isPositionNearEnoughToWin(
+			CGPointMake(100, 100),
+			itemData: self.matchingItemData!)
+		)
+		XCTAssertTrue(self.matchingItemController!.isPositionNearEnoughToWin(
+			CGPointMake(200, 200),
+			itemData: self.matchingItemData!)
+		)
+		XCTAssertTrue(self.matchingItemController!.isPositionNearEnoughToWin(
+			CGPointMake(300, 300),
+			itemData: self.matchingItemData!)
+		)
+		XCTAssertTrue(self.matchingItemController!.isPositionNearEnoughToWin(
+			CGPointMake(400, 400),
+			itemData: self.matchingItemData!)
+		)
+		XCTAssertFalse(self.matchingItemController!.isPositionNearEnoughToWin(
+			CGPointMake(500, 500),
+			itemData: self.matchingItemData!)
+		)
+	}
+	
 }
